9947e8b0b82cb65726b58aefc98210cc00ca8522
gomaeu <samuel.malicky2@gmail.com>
2026-09-03 22:58:57 +0200

    Publish complete EN DE ES and Chinese pages without runtime translation

---
+++ _next/static/Nk3nI0WPhbP4cfswT2eNp/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[[...path]]","\u002Fde\u002F[[...path]]","\u002Fes\u002F[[...path]]","\u002Fzh-CN\u002F[[...path]]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
